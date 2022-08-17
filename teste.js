80dfba5ed795bd4426b7dcabd52e8dcf855941b4
José Gonçalves <joseinacio774@gmail.com>
2022-08-17 17:59:02 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -2,34 +2,19 @@ var alt = 181
 var pess = 80
 
 s = (alt * alt) / pess
-if ((s >= 16 && s < 16, 9)) {
-  res.innerHTML = `Seu pelso IMC é ${s}, Esta Muito abaixo do peso!`
-} else if ((s >= 17 && s < 18, 9)) {
-  res.innerHTML = `Seu pelso IMC é ${s}, Esta abaixo do peso!`
-} else if ((s >= 18, 5 && s < 24, 9)) {
-  res.innerHTML = `Seu pelso IMC é ${s}, Peso Normla`
-} else if ((s >= 25 && s < 29, 9)) {
-  res.innerHTML = `Seu pelso IMC é ${s}, Acima do peso`
-} else if ((s >= 30 && s < 34, 9)) {
-  res.innerHTML = `Seu pelso IMC é ${s}, Obesidade Grau I`
-} else if (s >= 35 && s < 40) {
-  res.innerHTML = `Seu pelso IMC é ${s}, Obesidade Grau II`
-} else {
-  res.innerHTML = `Seu pelso IMC é ${s}, Obesidade Grau III`
-}
 
 if ((s >= 16 && s < 16, 9)) {
-  res.inerHTML = `Seu pelso IMC é ${s}, Esta Muito abaixo do peso!`
+  console.log(`Seu pelso IMC é ${s}, Esta Muito abaixo do peso!`)
 } else if ((s >= 17 && s < 18, 9)) {
-  res.inerHTML = `Seu pelso IMC é ${s}, Esta abaixo do peso!`
+  console.log(`Seu pelso IMC é ${s}, Esta abaixo do peso!`)
 } else if ((s >= 18, 5 && s < 24, 9)) {
-  res.innerHTML = `Seu pelso IMC é ${s}, Peso Normla`
+  console.log(`Seu pelso IMC é ${s}, Peso Normla`)
 } else if ((s >= 25 && s < 29, 9)) {
-  res.inerHTML = `Seu pelso IMC é ${s}, Acima do peso`
+  console.log = `Seu pelso IMC é ${s}, Acima do peso`
 } else if ((s >= 30 && s < 34, 9)) {
-  res.inerHTML = `Seu pelso IMC é ${s}, Obesidade Grau I`
+  console.log(`Seu pelso IMC é ${s}, Obesidade Grau I`)
 } else if (s >= 35 && s < 40) {
-  res.innerHTML = `Seu pelso IMC é ${s}, Obesidade Grau II`
+  console.log(`Seu pelso IMC é ${s}, Obesidade Grau II`)
 } else {
-  res.inerHTML = `Seu pelso IMC é ${s}, Obesidade Grau III`
+  console.log(`Seu pelso IMC é ${s}, Obesidade Grau III`)
 }
