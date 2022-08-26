d15df7a561d9b53918ecc379dbc70fb4c88a165c
José Gonçalves <joseinacio774@gmail.com>
2022-08-26 11:11:33 -0300

    add img
    Conforme seleciona o sexo seleciona também a imagem a a ser mostrada

--- script.js
+++ script.js
@@ -26,22 +26,72 @@ function handleButtomClick() {
   imc_result.textContent = formated_imc
 
   if (imc < 16.9) {
-    msg.innerHTML = `Você se encontra Muito Abaixo do Peso`
+    msg.innerHTML = `Você está Muito Abaixo do Peso`
   } else if (imc > 17 && imc <= 18.49) {
-    msg.innerHTML = `Você se encontra Abaixo do Peso`
+    msg.innerHTML = `Você está Abaixo do Peso`
   } else if (imc > 18.5 && imc <= 24.99) {
     msg.innerHTML = `Você se encontra no Peso Normal`
   } else if (imc > 25 && imc <= 29.99) {
-    msg.innerHTML = `Você se encontra Acima do Peso`
+    msg.innerHTML = `Você está Acima do Peso`
   } else if (imc > 30 && imc <= 34.99) {
-    msg.innerHTML = `Você se encontra em Obesidade Grau I`
+    msg.innerHTML = `Você está em Obesidade Grau I`
   } else if (imc > 35 && imc <= 40) {
-    msg.innerHTML = `Você se encontra Obesidade Grau II`
-  } else if (imc > 40 && imc < 100) {
-    msg.innerHTML = `Você se encontra Obesidade Grau III`
+    msg.innerHTML = `Você está em  Obesidade Grau II`
+  } else if (imc > 40 && imc < 500) {
+    msg.innerHTML = `Você está em  Obesidade Grau III`
   } else {
     msg.innerHTML = `Caucule agora seu Imc`
   }
+
+  //Add uma imagem
+  var fsex = document.getElementsByName('radsex')
+  var img = document.createElement('img')
+  var br = document.createElement('br')
+  img.setAttribute('id', 'foto')
+  var genero = ''
+  if (fsex[0].checked) {
+    genero = 'Homem'
+    if (imc < 16.9) {
+      img.setAttribute('src', './img/abaixo-h.png')
+    } else if (imc > 17 && imc <= 18.49) {
+      img.setAttribute('src', './img/abaixo-h.png')
+    } else if (imc > 18.5 && imc <= 24.99) {
+      img.setAttribute('src', './img/normal-h.png')
+    } else if (imc > 25 && imc <= 29.99) {
+      img.setAttribute('src', './img/obesidade-h.png')
+    } else if (imc > 30 && imc <= 34.99) {
+      img.setAttribute('src', './img/obesidade-h.png')
+    } else if (imc > 35 && imc <= 40) {
+      img.setAttribute('src', './img/obesidade-h+.png')
+    } else if (imc > 40 && imc < 500) {
+      img.setAttribute('src', './img/obesidade-h+.png')
+    } else {
+    }
+  }
+  if (fsex[1].checked) {
+    genero = 'Mulher'
+    if (imc < 16.9) {
+      img.setAttribute('src', './img/abaixo-m.png')
+    } else if (imc > 17 && imc <= 18.49) {
+      img.setAttribute('src', './img/cabaixo-m.png')
+    } else if (imc > 18.5 && imc <= 24.99) {
+      img.setAttribute('src', './img/normal-m.png')
+    } else if (imc > 25 && imc <= 29.99) {
+      img.setAttribute('src', './img/obesidade-m.png')
+    } else if (imc > 30 && imc <= 34.99) {
+      img.setAttribute('src', './img/obesidade-m.png')
+    } else if (imc > 35 && imc <= 40) {
+      img.setAttribute('src', './img/obesidade-m+.png')
+    } else if (imc > 40 && imc < 500) {
+      img.setAttribute('src', './img/obesidade-m+.png')
+    } else {
+    }
+  } else {
+  }
+
+  //adiconar a imagem dentro da div resultado  e alinhar conforme citado
+  ress.style.textAlign = 'center'
+  ress.appendChild(img)
 }
 
 start()
